dbaa8ccb6e95c638eec7df0e229caf891984c351
c-goldcrane <c.goldcrane@gmail.com>
2025-04-02 13:51:41 +0900

    chore: rollup 플러그인 설치 및 console 제거

--- packages/search-bar-ui/rollup.config.js
+++ packages/search-bar-ui/rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/index.ts",
@@ -31,6 +32,12 @@ export default {
     postcss({
       extract: false,
       modules: true,
+    }),
+    terser({
+      compress: {
+        drop_console: true,
+        pure_funcs: ["console.log"],
+      },
     }),
   ],
 };
